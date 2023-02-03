53a00943d97d1eab7d90a6abd076a2e7e990e9fd
Major Hayden <major@mhtx.net>
2023-02-03 14:19:41 -0600

    Get images in parallel

--- getimages.sh
+++ getimages.sh
@@ -7,7 +7,7 @@ aws ec2 describe-regions --filters Name=opt-in-status,Values=opted-in,opt-in-not
 for REGION in $(cat regions.txt); do
   aws --region=${REGION} ec2 describe-images \
     --filters Name=is-public,Values=true \
-    | jq '.Images | sort_by(.CreationDate)' > ${REGION}.json
+    | jq '.Images | sort_by(.CreationDate)' > ${REGION}.json &
 done
 
 xz -T0 -9 -v *.json
