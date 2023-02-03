8e5410b581ef985894870f99a12fcb4fa99aa11c
Major Hayden <major@mhtx.net>
2023-02-03 13:58:10 -0600

    Test downloading image data

---
+++ getimages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+aws ec2 describe-regions --filters Name=opt-in-status,Values=opted-in,opt-in-not-required \
+    | jq -r '.Regions[].RegionName' | sort > regions.txt
+
+for REGION in $(cat regions.txt); do
+  aws --region=${REGION} ec2 describe-images \
+    --filters Name=is-public,Values=true \
+    | jq '.Images | sort_by(.CreationDate)' > ${REGION}.json
+done
+
+xz -T0 -9 -v *.json
+ls -alh
